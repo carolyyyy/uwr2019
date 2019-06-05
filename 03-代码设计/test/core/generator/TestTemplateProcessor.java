baf0fc9f2532fdede66f09b8c6504d9482709b19
fyrui1128 <525127696@qq.com>
2019-06-05 23:54:41 +0800

    修改：     "03-\344\273\243\347\240\201\350\256\276\350\256\241/test/core/generator/TestTemplateProcessor.java" 	新文件：   "03-\344\273\243\347\240\201\350\256\276\350\256\241/test/core/generator/stale_outputs_checked" 	修改：     README.md

--- 03-代码设计/test/core/generator/TestTemplateProcessor.java
+++ 03-代码设计/test/core/generator/TestTemplateProcessor.java
@@ -16,7 +16,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-@RunWith(PowerMockRunner.class)
+ @RunWith(PowerMockRunner.class)
 @PrepareForTest(DataSourceConfig.class)
 @PowerMockIgnore("javax.management.*")
 public class TestTemplateProcessor implements DataSourceType{
